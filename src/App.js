1809cbe3ce8d7d719427f92cd37c441657b67140
bomibear <bomibearly@gmail.com>
2019-09-12 11:28:15 -0700

    add pic and readme

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component, Fragment } from "react";
 import "./app.scss";
 import Tasks from "./Tasks";
 import Nav from "./Nav";
-// http://taskmaster-dev.us-east-1.elasticbeanstalk.com/api/v1
-// http://taskmaster-env.3nz9fretef.us-west-2.elasticbeanstalk.com/
 
 class App extends Component {
   render() {
