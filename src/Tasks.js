389aa783c5027cbca61ca188fc40db44cf3f5a53
bomibear <bomibearly@gmail.com>
2019-09-14 14:29:44 -0700

    can upload file to S3.  no console errors

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, Component, Fragment } from "react";
-
+import React, { useState, useEffect, Fragment } from "react";
+let form = new FormData();
 const API =
-  "http://taskmaster.6jwvzatvsi.us-west-2.elasticbeanstalk.com/api/v1/tasks";
+  // "http://taskmaster.6jwvzatvsi.us-west-2.elasticbeanstalk.com/api/v1/tasks";
+  "http://localhost:5000/api/v1/tasks/";
 function Tasks() {
   useEffect(() => {
     fetchTasks();
@@ -15,6 +16,22 @@ function Tasks() {
     setItem(items);
   };
 
+  function _handleChange(event) {
+    let value = event.target.files ? event.target.files[0] : event.target.value;
+    form.set(event.target.name, value);
+  }
+
+  function _upload(event) {
+    event.preventDefault();
+    fetch(API + event.target.id.value + "/images", {
+      method: "POST",
+      mode: "no-cors",
+      body: form
+    })
+      .catch(error => console.error("Error:", error))
+      .then(response => console.log("Success:", response));
+  }
+
   return (
     <Fragment>
       <div className="App">
@@ -27,6 +44,18 @@ function Tasks() {
                     <span>{task.title}</span>
                   </summary>
                   <ol>
+                    <form
+                      onSubmit={_upload}
+                      action={API + task.id + "/images"}
+                      method="POST"
+                      encType="multipart/form-data"
+                    >
+                      <span>Upload Pic</span>
+                      <input onChange={_handleChange} name="file" type="file" />
+                      <button name="id" value={task.id}>
+                        Submit
+                      </button>
+                    </form>
                     {task.history.map((record, idx) => {
                       return (
                         <li key={idx}>
