18c36cd64667859dbd147233d9659c0307931993
bomibear <bomibearly@gmail.com>
2019-09-20 11:46:05 -0700

    can add a task with form to the api gateway

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, Fragment } from "react";
 let form = new FormData();
 const API =
-  "http://taskmaster.6jwvzatvsi.us-west-2.elasticbeanstalk.com/api/v1/tasks/";
+  // "http://taskmaster.6jwvzatvsi.us-west-2.elasticbeanstalk.com/api/v1/tasks/";
+  "https://trjrww9576.execute-api.us-west-2.amazonaws.com/prod/tasks/";
 
 const APIWithResizedImg = "https://alltasksresized.s3-us-west-2.amazonaws.com/";
 function Tasks() {
@@ -9,6 +10,7 @@ function Tasks() {
     fetchTasks();
   }, []);
   const [item, setItem] = useState([]);
+  const [formData, setFormData] = useState({});
 
   const fetchTasks = async () => {
     const data = await fetch(API);
@@ -17,9 +19,25 @@ function Tasks() {
     setItem(items);
   };
 
-  function _handleChange(event) {
+  function _handleChangeForUpload(event) {
     let value = event.target.files ? event.target.files[0] : event.target.value;
     form.set(event.target.name, value);
+  }
+
+  function _handleChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+  function _handleSubmit(event) {
+    event.preventDefault();
+    fetch(API, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .catch(error => console.error("Error:", error))
+      .then(response => console.log("Success:", response.body));
   }
 
   function _upload(event) {
@@ -36,6 +54,40 @@ function Tasks() {
   return (
     <Fragment>
       <div className="App">
+        <h2>Add a Task</h2>
+        <form onSubmit={_handleSubmit}>
+          <label>
+            <span>Title</span>
+            <input
+              onChange={_handleChange}
+              name="title"
+              type="text"
+              placeholder="title"
+              required
+            />
+          </label>
+          <label>
+            <span>Description</span>
+            <input
+              onChange={_handleChange}
+              name="description"
+              type="text"
+              placeholder="description"
+              required
+            />
+          </label>
+          <label>
+            <span>Assignee</span>
+            <input
+              onChange={_handleChange}
+              name="assignee"
+              type="text"
+              placeholder="assignee"
+            />
+          </label>
+          <button>Save</button>
+        </form>
+
         <ul>
           {item.map((task, idx) => {
             return (
@@ -66,7 +118,7 @@ function Tasks() {
                       >
                         <span>Upload Pic</span>
                         <input
-                          onChange={_handleChange}
+                          onChange={_handleChangeForUpload}
                           name="file"
                           type="file"
                         />
